3d317db7ead38cd5a8deb2981688113e8849c05f
Ryan Rueber <ruebr432@CE105WD554105S.hillnet.ad.hsd.k12.or.us>
2015-03-05 09:22:14 -0800

    Updated with provided template

--- CYOA.py
+++ CYOA.py
@@ -1,14 +1,73 @@
-#CYOA.py
-#By: Chris Renslow, Ryan Rueber, and Stetson Jones
+# Choose.py
+# by Ryan Rueber, Chris Renslow, and Stetson Jones
+# Description: An adventure about a heist.
 
-import random
+
+# Import Statements
 from tkinter import *
-import tkinter.simpledialog as simpledialog
-import tkinter.messagebox as messagebox
+import tkinter.simpledialog
+import tkinter.messagebox
+
 root = Tk()
+w = Label(root, text="Choose Your Own Adventure")
+w.pack()
 
-####Chris####
+def intro():
+    """ Introductory Function -> starts the story going """
+    messagebox.showinfo("Title", "\nHello, you are a _____ from ____. " + \
+                        "You stumble across such and such.")
+    choice = simpledialog.askinteger("Choose wisely",
+                                   "You have a choice to pick: 1 or 2.")
+    if choice == 1:
+        choice1()
+    elif choice == 2:
+        choice2()
+    else:
+        intro()
 
-####Ryan####
+################ Ryan Rueber Functions #####################
+def choice1():
+    choice = simpledialog.askinteger("Choose wisely",
+                                     "This is the next part of the story.  Now you must choose 1 or 2 again.")
+    if (choice == 1):
+        messagebox.showinfo("The End",
+                            "You chose right.  THE END")
 
-#####Stetson####
+    elif (choice == 2):
+        messagebox.showinfo("The End",
+                            "You chose ok.  THE END")
+    else:
+        choice1()
+
+################ Chris Renslow Functions #####################
+def choice2():
+    choice = simpledialog.askinteger("Choose wisely",
+                                     "This is the next part of the story.  Now you must choose 1 or 2 again.")
+    if (choice == 1):
+        messagebox.showinfo("The End",
+                            "You chose right.  THE END")
+
+    elif (choice == 2):
+        messagebox.showinfo("The End",
+                            "You chose ok.  THE END")
+    else:
+        choice2()
+        
+################ Stetson Jones Functions #####################
+def choice2():
+    choice = simpledialog.askinteger("Choose wisely",
+                                     "This is the next part of the story.  Now you must choose 1 or 2 again.")
+    if (choice == 1):
+        messagebox.showinfo("The End",
+                            "You chose right.  THE END")
+
+    elif (choice == 2):
+        messagebox.showinfo("The End",
+                            "You chose ok.  THE END")
+    else:
+        choice2()
+
+################ Main #####################
+intro()
+
+root.destroy()
